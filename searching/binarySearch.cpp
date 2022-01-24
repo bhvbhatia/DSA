e88ab36ba5dae7b2432fcbd6e51c0707a0bb42f7
arvy <arvy@example.com>
2022-01-24 17:01:51 +0530

    binary search

--- searching/binarySearch.cpp
+++ searching/binarySearch.cpp
@@ -6,7 +6,7 @@ int binary(int arr[], int size, int key)
     int start = 0;
     int end = size - 1;
 
-    int mid = (start + end) / 2;
+    int mid = start + (end - start) / 2;
     while (start <= end)
     {
         if (arr[mid] == key)
@@ -21,7 +21,7 @@ int binary(int arr[], int size, int key)
         {
             end = mid - 1;
         }
-        mid = (start + end) / 2;
+        mid = start + (end - start) / 2;
     }
     return -1;
 }
