80f7016419e2c41a13f79be2a74fb312dc0acc12
arvy <arvy@example.com>
2022-01-25 09:20:12 +0530

    FirstandLastOcc

---
+++ searching/FirstandLastOccu.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Q. find first and last occurence of element in a given sorted array.
+// codestudio problem
+
+int firstocc(int arr[], int size, int key)
+{
+
+    int s = 0, e = size - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+int firstocc(int arr[], int size, int key)
+{
+
+    int s = 0, e = size - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int lastocc(int arr[], int size, int key)
+{
+
+    int s = 0, e = size - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    int even[5] = {1, 2, 3, 3, 5};
+    cout << firstocc(even, 5, 3) << " ";
+    cout << lastocc(even, 5, 3);
+    return 0;
+}
