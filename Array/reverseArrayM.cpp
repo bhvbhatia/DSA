2fb59a60d2d8a79f5f2b171f1aaf37c7c7d83cb7
arvy <arvy@example.com>
2022-02-10 10:57:30 +0530

    reverse

---
+++ Array/reverseArrayM.cpp
@@ -0,0 +1,14 @@
+// codestudio question
+// reverse array after M idex
+
+void reverseArray(vector<int> &arr, int m)
+{
+    // Write your code here.
+    int s = m + 1, e = arr.size() - 1;
+    while (s <= e)
+    {
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+}
