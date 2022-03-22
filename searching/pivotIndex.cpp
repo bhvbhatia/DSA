7c2e2f821cdc38812388f39468bfc5cd131acda4
arvy <arvy@example.com>
2022-03-22 12:23:51 +0530

    pivotIndex

--- searching/pivotIndex.cpp
+++ searching/pivotIndex.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
 int getPivot(int arr[], int n)
 {
     int s = 0, e = n - 1;
@@ -20,11 +20,29 @@ int getPivot(int arr[], int n)
     }
     return s;
 }
+*/
+// recursive approach
 
+int getPivot(int arr[], int low, int high)
+{
+    int mid = low + (high - low) / 2;
+    if (low >= high)
+    {
+        return low;
+    }
+    if (arr[mid] >= arr[0])
+    {
+        return getPivot(arr, mid + 1, high);
+    }
+    else
+    {
+        return getPivot(arr, low, mid);
+    }
+}
 int main()
 {
 
     int ar[5] = {3, 8, 10, 17, 1};
-    cout << getPivot(ar, 5);
+    cout << getPivot(ar, 0, 4);
     return 0;
 }
