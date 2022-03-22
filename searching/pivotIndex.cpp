efe6420adf4d490bdd75705c1d7a1846f5c90662
arvy <arvy@example.com>
2022-03-22 12:15:46 +0530

    pivotIndex

---
+++ searching/pivotIndex.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getPivot(int arr[], int n)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+int main()
+{
+
+    int ar[5] = {3, 8, 10, 17, 1};
+    cout << getPivot(ar, 5);
+    return 0;
+}
